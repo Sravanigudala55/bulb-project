15be1472fb2f6ebcf1dc4672c147204da6f18510
Sravanigudala55 <137883533+Sravanigudala55@users.noreply.github.com>
2023-07-19 16:04:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function nature1(){
+    document.getElementById("nature").src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg";
+  }
+  function nature2(){
+      document.getElementById("nature").src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_640.jpg";
+    }
+    function nature3(){
+      document.getElementById("nature").src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_640.jpg";
+    }
